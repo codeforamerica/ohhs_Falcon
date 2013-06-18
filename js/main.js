6a3096b55c0c14671a701383f5752df71a23342b
Michal Migurski <mike@teczno.com>
2013-06-18 15:21:05 -0700

    Starting from a less-zoomed SF for automatic location

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var map;
 $(function(){
   
-  map = L.map('map').setView([37.7749295, -122.4494155], 17);
+  map = L.map('map').setView([37.767745, -122.441475], 12);
   var mapquestUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
   subDomains = ['otile1','otile2','otile3','otile4'],
   mapquestAttrib = 'Data by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
   var mapquest = new L.TileLayer(mapquestUrl, {maxZoom: 19, attribution: mapquestAttrib, subdomains: '1234'});
 
   map.addLayer(mapquest);
-  map.locate({setView:true, maxZoom:19})
+  map.locate({setView:true, maxZoom:19});
 
   var style = {
     "clickable": true,
